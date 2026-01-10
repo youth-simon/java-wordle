a22d553020751ab444acf1148dd1c9a6f3928154
hyojin <kimhj9292@gmail.com>
2026-01-10 17:53:45 +0900

    (feature): TrailCounterTest 테스트코드 추가 WordComparatorTest 테스트코드 추가

--- src/test/java/model/TrialCounterTest.java
+++ src/test/java/model/TrialCounterTest.java
@@ -3,6 +3,8 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrialCounterTest {
@@ -10,16 +12,32 @@ class TrialCounterTest {
     @Test
     @DisplayName("increment 는 trials가 1 증가한다.")
     void increment() {
+        TrialCounter trialCounter = new TrialCounter();
+
+        trialCounter.increment();
+        assertEquals(trialCounter.getTrials(), 1);
+
+        trialCounter.increment();
+        assertEquals(trialCounter.getTrials(), 2);
     }
 
     @Test
-    @DisplayName("최대 6회 미만이고 keepTrying 이면 true 를 반환한다.")
+    @DisplayName("trials가 6회 미만이고 keepTrying 이면 true 를 반환한다.")
     void keepTryingIsTrue() {
+        TrialCounter trialCounter = new TrialCounter();
+        trialCounter.increment();
+        trialCounter.increment();
+
+        assertTrue(trialCounter.keepTrying());
     }
 
     @Test
-    @DisplayName("최대 6회 미만이면 true 를 반환한다.")
+    @DisplayName("trials가 6회를 초과하면 false 를 반환한다.")
     void keepTryingIsFalse() {
+        TrialCounter trialCounter = new TrialCounter();
+        for (int i = 0; i < 7; i++) {
+            trialCounter.increment();
+        }
+        assertFalse(trialCounter.keepTrying());
     }
-
 }
--- src/test/java/model/WordComparatorTest.java
+++ src/test/java/model/WordComparatorTest.java
@@ -10,31 +10,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class WordComparatorTest {
 
     @Test
-    @DisplayName("완전일치 ")
+    @DisplayName("getPerfectMathcedIndex()에서 모든 글자가 일치할 경우 단어의 전체 인덱스를 반환한다.")
     void fullyMatch_getPerfectMathcedIndex() {
         WordComparator wordComparator = new WordComparator();
 
-        List<Integer> partial1 = wordComparator.getPerfectMathcedIndex("aaaaa", "aaaaa");
-        assertEquals(partial1, List.of(0,1,2,3,4));
+        List<Integer> perfect1 = wordComparator.getPerfectMathcedIndex("apple", "apple");
+        assertEquals(perfect1, List.of(0,1,2,3,4));
     }
 
     @Test
-    @DisplayName("부분일치 ")
+    @DisplayName("getPerfectMathcedIndex()에서 특정 글자가 완전일치할 경우 완전일치하는 글자 인덱스만 반환한다.")
     void partialMatch_getPerfectMathcedIndex() {
+        WordComparator wordComparator = new WordComparator();
+
+        List<Integer> partial1 = wordComparator.getPerfectMathcedIndex("appkk", "apple");
+        assertEquals(partial1, List.of(0,1,2));
     }
 
     @Test
-    @DisplayName("일치하는 부분 없을때 ")
+    @DisplayName("getPerfectMathcedIndex()에서 완전일치하는 글자가 없을때는 빈 리스트를 반환한다.")
     void noMatch_getPerfectMathcedIndex() {
+        WordComparator wordComparator = new WordComparator();
+
+        List<Integer> noMatch1 = wordComparator.getPerfectMathcedIndex("kkkkk", "apple");
+        assertEquals(noMatch1, List.of());
     }
 
     @Test
-    @DisplayName("부분일치 일때, ")
+    @DisplayName("getPartialMatchedIndex()에서 모든 글자가 부분일치 할 경우 단어의 전체 인덱스를 반환한다.")
+    void fullyMatch_getPartialMatchedIndex() {
+        WordComparator wordComparator = new WordComparator();
+
+        List<Integer> perfect1 = wordComparator.getPartialMatchedIndex("leapp", "apple");
+        assertEquals(perfect1, List.of(0,1,2,3,4));
+    }
+
+    @Test
+    @DisplayName("getPartialMatchedIndex()에서 특정 글자가 부분일치할 경우 부분일치하는 글자 인덱스만 반환한다.")
     void partialMatch_getPartialMatchedIndex() {
+        WordComparator wordComparator = new WordComparator();
+
+        List<Integer> partial1 = wordComparator.getPartialMatchedIndex("kkkpp", "apple");
+        assertEquals(partial1, List.of(3,4));
     }
 
     @Test
-    @DisplayName("일치하는 부분이 없을때, ")
+    @DisplayName("getPartialMatchedIndex()에서 부분일치하는 글자가 없을때는 빈 리스트를 반환한다.")
     void noMatch_getPartialMatchedIndex() {
+        WordComparator wordComparator = new WordComparator();
+
+        List<Integer> noMatch1 = wordComparator.getPartialMatchedIndex("kkkkk", "apple");
+        assertEquals(noMatch1, List.of());
     }
 }
