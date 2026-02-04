e869070e0e856341d4ed0be5cc42b42ee6470f2e
hyojin <kimhj9292@gmail.com>
2026-02-04 23:02:04 +0900

    (refactor): WordResult 수정 - 글자 완전 일치를 판단할 수 있는 isPerfect() 메서드 추가

--- src/main/java/model/WordResult.java
+++ src/main/java/model/WordResult.java
@@ -6,8 +6,15 @@ import java.util.List;
 public class WordResult {
 
     private final String[] tiles;
+    private final int wordLength;
+    private final List<Integer> partialMatchedIndexes ;
+    private final List<Integer> perfectlyMatchedIndexes;
 
     public WordResult(int wordLength, List<Integer> partialIndexes, List<Integer> perfectIndexes) {
+        this.wordLength = wordLength;
+        this.partialMatchedIndexes  = partialIndexes;
+        this.perfectlyMatchedIndexes = perfectIndexes;
+
         tiles = new String[wordLength];
 
         Arrays.fill(tiles, TileColor.WHITE.getEmoji());
@@ -19,6 +26,10 @@ public class WordResult {
         for (int index : perfectIndexes) {
             tiles[index] = TileColor.GREEN.getEmoji();
         }
+    }
+
+    public boolean isPerfect() {
+        return perfectlyMatchedIndexes.size() == wordLength;
     }
 
     public String[] getTiles() {
